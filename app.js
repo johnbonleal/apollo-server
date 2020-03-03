74ca9fc62dcfd72074cf4f7f365cf108c974270f
John Leal <johnbon.leal@outlook.com>
2020-03-03 15:26:03 +0800

    Implemented express

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
-const {ApolloServer} = require('apollo-server');
-const typeDefs = require('./src/schema/schema');
-const resolvers = require('./src/resolvers/resolver');
+import {ApolloServer} from 'apollo-server-express';
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import typeDefs from './src/schema/schema';
+import resolvers from './src/resolvers';
+import config from './src/configs/app-config';
+
+import bookModel from './src/db/model/book.model';
+import authorModel from './src/db/model/author.model';
+
+const {host, db_port, name} = config.mongo;
+
+const app = express();
+app.use(cors());
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: () => ({
+    models: {bookModel, authorModel}
+  })
 });
 
-server.listen().then(({url}) => {
-  console.log(`Server listening at ${url}`);
+server.applyMiddleware({app});
+
+app.listen({port: config.app.port}, () => {
+  mongoose.connect(`mongodb://${host}:${db_port}/${name}`, { useNewUrlParser: true, useUnifiedTopology: true });
+  console.log(`Server running at http://localhost:${config.app.port}/${server.graphqlPath}`);
 });
